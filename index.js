902ffdb69062df2bc0dd65fd5f3b49274a794efe
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-02-02 19:30:46 +0000

    amended structure and added gitignore

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
